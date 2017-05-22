4c4a9c6d6141accca55bec748c86e9caeaf2bd66
yousha arif <yousha arif>
2017-05-22 06:30:10 +0500

    First commit

---
+++ SM/SM/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace SM
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(18, 12);
+            this.progressBar1.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(434, 40);
+            this.progressBar1.TabIndex = 0;
+            this.progressBar1.Click += new System.EventHandler(this.progressBar1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(20, 69);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(278, 72);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Your computer will automatically\r\n Shutdown \r\nTo abort click postpone.\r\n";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(324, 120);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(128, 41);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Postpone";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "1 minute",
+            "10 minutes",
+            "20 minutes",
+            "30 minutes"});
+            this.comboBox1.Location = new System.Drawing.Point(324, 74);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(124, 21);
+            this.comboBox1.TabIndex = 3;
+            this.comboBox1.Text = "10 minutes";
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 1000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(473, 188);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ SM/SM/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SM
+{
+    public partial class Form1 : Form
+    {
+        public int i = 10; // Seconds before form  prompts
+        
+        public Form1()
+        {    
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            // NULL
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+            //NULL
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized; // intial state of form when loaded
+            this.ShowInTaskbar = false; // should not be visible in taskbar
+            timer2.Start(); // timer for the form to prompt
+          
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            try
+            { 
+                progressBar1.Value += 3; // for every second make 3 % progress
+            }
+            catch
+            {
+                progressBar1.Value += 1; // when value = 99% , only increase 1% 
+                if (progressBar1.Value == 100)
+                {
+                    System.Diagnostics.Process.Start("Shutdown.exe ", "-s -f -t 0"); //shutdown commmad with 0 second delay, and forcing all apps to close.
+                }
+
+                timer1.Stop(); // turn off the timers when job done 
+            }
+           }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            progressBar1.Value = 0;          //For postpone 
+            timer1.Stop();                   //next value of i will be seconds of delay 
+            timer2.Start();
+            this.WindowState = FormWindowState.Minimized;
+            this.ShowInTaskbar = false;
+            if (comboBox1.Text == "1 minute")
+            {
+                i = 60 ;
+            }
+            else if (comboBox1.Text == "10 minutes")
+            {
+                i = 600;
+            } 
+            else if (comboBox1.Text == "20 minutes")
+            {
+                i = 1200;
+            }
+            else if (comboBox1.Text == "30 minutes")
+            {
+                i = 1800;
+            }
+
+
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            i--; //decrease one second
+            if (i == 0)
+            {
+                this.WindowState = FormWindowState.Normal; //prompt up
+                this.ShowInTaskbar = true;
+                timer1.Start();
+                timer2.Stop();
+            }
+
+        }
+    }
+}
